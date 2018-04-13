250d8677bb97ccabb4b90251b5e9c83544f39a0a
Aureleb <38351818+Aureleb@users.noreply.github.com>
2018-04-13 17:02:58 +0200

    Add files via upload
    ajout des fonctions sidestepL,sidestepR,insulte,shoot et ajout de variables d'instances et d'importations de modules.

--- py/team1/nao.py
+++ py/team1/nao.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 from naoqi import ALProxy
-
+import sys
+import motion
+import time
+import math
+import numpy as np
 
 class Nao:
     def __init__(self, ip, port):
         self.motion = ALProxy('ALMotion', ip, int(port))
         self.sonar = ALProxy('ALSonar', ip, int(port))
         self.memory = ALProxy('ALMemory', ip, int(port))
-
+        self.postureProxy = ALProxy("ALRobotPosture", ip, int(port))
         self.sonar.subscribe('nao')
+        self.tts = ALProxy("ALTextToSpeech", ip, int(port))
 
         self.motion.wakeUp()
 
@@ -49,3 +54,114 @@ class Nao:
 
     def walk_back(self):
         self.motion.move(-0.1, 0, 0)
+    
+    def sidestepR(self):
+        footStepsList=[]
+        footStepsList.append([["RLeg"], [[0.00, -0.16, 0.0]]])
+        footStepsList.append([["LLeg"], [[0.00, 0.1, 0.0]]])
+        stepFrequency = 0.8
+        clearExisting = False
+        for i in range( len(footStepsList) ):
+            self.motion.setFootStepsWithSpeed(
+                footStepsList[i][0],
+                footStepsList[i][1],
+                [stepFrequency],
+                clearExisting)
+    def sidestepL(self):
+        footStepsList=[]
+        footStepsList.append([["LLeg"], [[0.00, 0.16, 0.0]]])
+        footStepsList.append([["RLeg"], [[0.00, -0.1, 0.0]]])
+        stepFrequency = 0.8
+        clearExisting = False
+        for i in range( len(footStepsList) ):
+            self.motion.setFootStepsWithSpeed(
+                footStepsList[i][0],
+                footStepsList[i][1],
+                [stepFrequency],
+                clearExisting)
+    def insulte(self):
+        L=["Ma grand-mère joue mieux que toi","Ba alors, casse toi pauvre con", "Allez Paris", "Vous n'avez pas honte?", "Ba alors, on attend pas Patrick?","Je suis pas chasseur mais je lui mettrai bien une cartouche", "Poulet braisé mamène", "Benoit Zère rappeur du finistère", "Benoit Zère mon dieu","Votez Troliste", "Send Nudes"]
+        a=np.random.randint(0,len(L))
+        self.tts.say(L[a])
+        
+    def shoot(self):
+        # Set NAO in Stiffness On
+        #StiffnessOn(proxy)
+    
+        # Send NAO to Pose Init
+        self.postureProxy.goToPosture("StandInit", 0.5)
+     
+        # Activate Whole Body Balancer
+        isEnabled  = True
+        self.motion.wbEnable(isEnabled)
+    
+        # Legs are constrained fixed
+        stateName  = "Fixed"
+        supportLeg = "Legs"
+        self.motion.wbFootState(stateName, supportLeg)
+    
+        # Constraint Balance Motion
+        isEnable   = True
+        supportLeg = "Legs"
+        self.motion.wbEnableBalanceConstraint(isEnable, supportLeg)
+    
+        # Com go to LLeg
+        supportLeg = "LLeg"
+        duration   = 2.0
+        self.motion.wbGoToBalance(supportLeg, duration)
+    
+        # RLeg is free
+        stateName  = "Free"
+        supportLeg = "RLeg"
+        self.motion.wbFootState(stateName, supportLeg)
+    
+        # RLeg is optimized
+        effectorName = "RLeg"
+        axisMask     = 63
+        space        = self.motion.FRAME_ROBOT
+    
+    
+        # Motion of the RLeg
+        dx      = 0.05                 # translation axis X (meters)
+        dz      = 0.05                 # translation axis Z (meters)
+        dwy     = 5.0*math.pi/180.0    # rotation axis Y (radian)
+    
+    
+        times   = [2.0, 2.7, 4.5]
+        isAbsolute = False
+    
+        targetList = [
+          [-dx, 0.0, dz, 0.0, +dwy, 0.0],
+          [+dx, 0.0, dz, 0.0, 0.0, 0.0],
+          [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+    
+        self.motion.positionInterpolation(effectorName, space, targetList,
+                                     axisMask, times, isAbsolute)
+    
+    
+        # Example showing how to Enable Effector Control as an Optimization
+        isActive     = False
+        self.motion.wbEnableEffectorOptimization(effectorName, isActive)
+    
+        # Com go to LLeg
+        supportLeg = "RLeg"
+        duration   = 2.0
+        self.motion.wbGoToBalance(supportLeg, duration)
+    
+        # RLeg is free
+        stateName  = "Free"
+        supportLeg = "LLeg"
+        self.motion.wbFootState(stateName, supportLeg)
+    
+        effectorName = "LLeg"
+        self.motion.positionInterpolation(effectorName, space, targetList,
+                                    axisMask, times, isAbsolute)
+    
+        time.sleep(1.0)
+    
+        # Deactivate Head tracking
+        isEnabled    = False
+        self.motion.wbEnable(isEnabled)
+    
+        # send robot to Pose Init
+        self.postureProxy.goToPosture("StandInit", 0.5)
